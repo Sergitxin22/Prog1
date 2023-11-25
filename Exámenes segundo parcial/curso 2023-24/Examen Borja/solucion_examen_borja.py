d63ed98577a221d2f0d703addc7248fa2cf47dea
Sergitxin22 <sergiohablador@gmail.com>
2023-11-25 21:57:48 +0100

    Add: Borja Exam Solution

--- Exámenes segundo parcial/curso 2023-24/Examen Borja/solucion_examen_borja.py
+++ Exámenes segundo parcial/curso 2023-24/Examen Borja/solucion_examen_borja.py
@@ -63,15 +63,83 @@ vuelos = [vuelo1, vuelo2, vuelo3, vuelo4, vuelo5]
 """
 EJERCICIO 1
 """
+def calculo_capacidad_aeropuerto(vuelos):
+    capacidad_disponible = 0
+    
+    return capacidad_disponible
+
+# print(calculo_capacidad_aeropuerto(vuelos)) # Devuelve 0.4
 
 """
 EJERCICIO 2
 """
+def agregar_pasajero(vuelos, num_vuelo, datos_pasajero):
+    for vuelo in vuelos:
+        numero_vuelo = vuelo['numero_vuelo']
+
+        if numero_vuelo == num_vuelo:
+            vuelo['pasajeros'].append(datos_pasajero)
+
+            print(f"{datos_pasajero['nombre']} ha sido agregada/o al vuelo {num_vuelo}.")
+            return None
+    
+    print(f"No se encontró el vuelo con número {num_vuelo}.")
+    return None
+
+# nuevo_pasajero = {"nombre": "Sofía Martín", "identificacion": "ID1010", "asiento": "12C"}
+# agregar_pasajero(vuelos, "AA123", nuevo_pasajero) # Salida: Sofía Martín ha sido agregada/o al vuelo AA123.
+# agregar_pasajero(vuelos, "AA124", nuevo_pasajero) # Salida: No se encontró el vuelo con número AA124.
 
 """
 EJERCICIO 3
 """
+def buscar_vuelo_por_destino(vuelos, destino):
+    vuelos_filtrados = []
+
+    for vuelo in vuelos:
+        destino_vuelo = vuelo['destino']
+
+        if destino == destino_vuelo:
+            vuelos_filtrados.append(vuelo)
+
+    return vuelos_filtrados
+
+# print(buscar_vuelo_por_destino(vuelos, "Miami")) # Devuelve [{'numero_vuelo': 'BB456', 'origen': 'Bogotá', 'destino': 'Miami', 'salida': '14:00', 'llegada': '18:00', 'pasajeros': [{'nombre': 'Luisa Fernández', 'identificacion': 'ID1003', 'asiento': '11A'}]}]
 
 """
 EJERCICIO 4
 """
+def calcular_itinerarios(vuelos, origen, destino):
+    itinerarios_encontrados = []
+
+    # Buscar vuelos directos
+    for vuelo in vuelos:
+        if vuelo['origen'] == origen and vuelo['destino'] == destino:
+            itinerarios_encontrados.append([vuelo])
+    
+    # Buscar vuelos con escalas
+    for vuelo1 in vuelos:
+        if vuelo1['origen'] == origen:
+            for vuelo2 in vuelos:
+                if vuelo1['destino'] == vuelo2['origen'] and vuelo2['destino'] == destino:
+                    itinerarios_encontrados.append([vuelo1, vuelo2])
+
+    # Mostrar los resultados
+    if itinerarios_encontrados:
+        primer_itinerario = itinerarios_encontrados[0][0]
+        segundo_itinerario = itinerarios_encontrados[0][1]
+        print(f"Itinerario: {primer_itinerario['numero_vuelo']} -> {segundo_itinerario['numero_vuelo']}")
+        
+        # Calcular tiempos
+        tiempo_entre_vuelos = format_hora(segundo_itinerario['salida']) - format_hora(primer_itinerario['llegada'])
+        tiempo_vuelo_1 = format_hora(primer_itinerario['llegada']) - format_hora(primer_itinerario['salida'])
+        tiempo_vuelo_2 = format_hora(segundo_itinerario['llegada']) - format_hora(segundo_itinerario['salida'])
+        tiempo_total_viaje = tiempo_vuelo_1 + tiempo_vuelo_2 + tiempo_entre_vuelos
+
+        print(f'Tiempo de espera entre vuelos: {tiempo_entre_vuelos}')
+        print(f'Tiempo total de viaje: {tiempo_total_viaje}')
+    else:
+        print("No se han encontrado conexiones para esas ciudades")
+
+calcular_itinerarios(vuelos, "Madrid", "Londres")
+calcular_itinerarios(vuelos, "Madrid", "Bilbao")
