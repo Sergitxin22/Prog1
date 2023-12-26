871100b190ac53bc65cb0e462ffd34c522257fca
Sergitxin22 <sergiohablador@gmail.com>
2023-12-26 19:59:33 +0100

    Add: Jenny Exam

---
+++ Exámenes primer parcial/curso 2023-24/Examen Jenny/solucion_examen_jenny.py
@@ -0,0 +1,46 @@
+LIBROS = [
+    ["001", "Cien años de soledad", "Gabriel Garcia Marquez", 1982, 6],
+    ["002", "El señor de los anillos", "J.R.R. Tolkien", 1954, 12],
+    ["003", "Un mundo feliz", "Aldous Huxley", 1932, 3],
+    ["004", "Orgullo y prejuicio", "Jane Austen", 1813, 1],
+    ["005", "Crimen y castigo", "Fiodor Dostoyevski", 1866, 2],
+    ["006", "Lolita", " Vladimir Nabokov", 1955, 8],
+    ["007", "Ulises", "James Joyce", 1920, 4],
+    ["008", "Madame Bovary", "Gustave Flaubert", 1857, 1],
+    ["009", "En busca del tiempo perdido", "Marcel Proust", 1914, 3],
+]
+
+PRESTAMOS = [
+    ["006", "Ana B. Lago", 20],
+    ["008", "Lidia Rodriguez", 15],
+    ["005", "Borja Sanz", 31],
+    ["002", "Mari Luz Guenaga", 31],
+    ["003", "Pablo Garaizar", 20],
+    ["005", "Xabier Cantero", 15],
+    ["001", "Mikel Emaldi", 15],
+    ["002", "Leire Gomez", 31],
+    ["002", "Carmen Sanchez", 15],
+    ["009", "Pedro Garcia", 31]
+]
+
+"""
+EJERCICIO 1
+"""
+
+
+"""
+EJERCICIO 2
+"""
+
+
+"""
+EJERCICIO 3
+"""
+
+"""
+EJERCICIO 4
+"""
+
+"""
+EJERCICIO EXTRA
+"""
